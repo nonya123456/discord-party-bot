105e5c5f18038d8fa3e7651cff45b68dd1d7859b
nonya123456 <nonya579@gmail.com>
2023-02-13 20:28:59 +0700

    Add config

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+var configOnce sync.Once
+var config *Config
+
+type Config struct {
+}
+
+func InitConfig() *Config {
+	configOnce.Do(func() {
+
+		viper.SetConfigName("config")            // name of config file without extension
+		viper.AddConfigPath("./internal/config") // path to look for config file, relative to working directory
+		viper.AddConfigPath("/config")           // production config mount path
+
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+		if err := viper.ReadInConfig(); err != nil {
+			panic(errors.Wrap(err, "Config file not found"))
+		}
+		viper.AutomaticEnv()
+
+		viper.WatchConfig() // Watch for changes to the configuration file and recompile
+		if err := viper.Unmarshal(&config); err != nil {
+			panic(errors.Wrap(err, "Cannot unmarshal config"))
+		}
+	})
+	return config
+}
